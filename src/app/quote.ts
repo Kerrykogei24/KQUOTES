557fc71a00696541de4d3f92f5293d4b9816ec39
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-03 01:41:52 +0300

    add constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,19 @@
+import { state } from '@angular/animations';
+
 export class Quote {
     id:number;
     author:string;
     state:string;
     user:string;
+    // upVote:number;
+    // downVote:number;
+
+    constructor(id:number, author:string, state:string,user:string,upVote:number, downVote:number,){
+        this.id=id;
+        this.author=author;
+        this.state=state;
+        this.user= user;
+        // this.upVote=0;
+        // this.downVote=0;
+    }
 }
