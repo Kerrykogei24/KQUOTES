36b9af55149b36559b7e08c37b44912574b76125
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-03 23:30:54 +0300

    modified quotes html

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -10,12 +10,12 @@ import { Quote } from '../quote';
 })
 export class QuotesComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote(1,'Eleanor Roosevelt','Learn from the mistakes of others. You cant live long enough to make them all yourself', ' Francian Mbugua', new Date(2020,11,1)),
-    new Quote(2,'Willie Nelson ','Once you replace negative thoughts with positive ones, youll start having positive results.', ' Benard Akaka', new Date(2020,11,3)),   
-    new Quote(3,' Charles Spurgeon ','It is not how much we have, but how much we enjoy, that makes happiness.', ' Cythia Oduol',new Date(2020,10,30)),    
-    new Quote(4,'W.E.B. Du Bois ','The most important thing to remember is this: to be ready at any moment to give up what you are for what you might become', ' Arnold Collins', new Date(2020,11,2)),
-    new Quote(5,' Michael Scofield',' choose to have faith, because, without that, I have nothing… It’s the only thing that’s keeping me going', ' Philipia Eve', new Date(2020,10,3)),  
-    new Quote(6,'Theodore Bagwell','We are captives of our own identities, living in prisons of our own creation.', ' Kerry Komar', new Date(2020,11,4))
+    new Quote(1,'Eleanor Roosevelt','"Learn from the mistakes of others. You cant live long enough to make them all yourself."', ' Francian Mbugua', new Date(2020,11,1)),
+    new Quote(2,'Willie Nelson ','"Once you replace negative thoughts with positive ones, youll start having positive results."', ' Benard Akaka', new Date(2020,11,3)),   
+    new Quote(3,' Charles Spurgeon ','"It is not how much we have, but how much we enjoy, that makes happiness."', ' Cynthia Oduol',new Date(2020,10,30)),    
+    new Quote(4,'W.E.B. Du Bois ','"The most important thing to remember is this: to be ready at any moment to give up what you are for what you might become"', ' Arnold Collins', new Date(2020,11,2)),
+    new Quote(5,' Michael Scofield','"Choose to have faith, because, without that, I have nothing… It’s the only thing that’s keeping me going"', ' Philipia Eve', new Date(2020,10,3)),  
+    new Quote(6,'Theodore Bagwell','"We are captives of our own identities, living in prisons of our own creation."', ' Kerry Komar', new Date(2020,11,4))
   ];
 
   likes(index) {
@@ -24,7 +24,7 @@ export class QuotesComponent implements OnInit {
   unlikes(index) {
     this.quotes[index].unlikes++;
   }
-  isComplete=true;
+  // isComplete=true;
 
   deleteQuote(isComplete, index) {
     if (isComplete) {
